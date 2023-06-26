febf02beae325e27abce2f2ab84d9751a3f59f9c
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-06-26 16:35:43 +0100

    update

--- duck.js
+++ duck.js
@@ -35,10 +35,10 @@ function renderProducts() {
     allProducts[product1].views++;
     allProducts[product2].views++;
   
-function handleProductClick(event);
-if (event.target === productContainer) {
+function handleProductClick(event) {
+    if (event.target === productContainer) {
     alert("please click on an image");
- } else {
+    } else {
     clicks++;
     console.log(clicks);
     let clickedProduct = event.target.alt;
@@ -56,9 +56,42 @@ if (clicks === maxClicksAllowed){
     resultsButton.className = "clicks-allowed";
 } else {
     renderProducts();
-
 }
 
+function renderResults(){
+    console.log("Your results are in!");
+    let ul = document.querySelector("ul");
+    for (let i = 0; i < allProducts.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${allProducts[i].name} had ${allProducts[i].views} views and was clicked ${allProducts[i].clicks} times.`;
+        ul.appendChild(li);
+    }
+ }
+
+} 
+
+const bag = new Product("R2D2 bag", "img/bag.jpg");
+const banana = new Product("banana slicer", "img/banana.jpg");
+const bathroom = new Product("bathroom tablet", "img/bathroom.jpg");
+const boots = new Product("open toe boots", "img/boots.jpg");
+const breakfast = new Product("breakfast maker", "img/breakfast.jpg");
+const bubblegum = new Product("meatball bubblegum", "img/bubblegum.jpg");
+const chair = new Product("inverted chair", "img/chair.jpg");
+const cthulu = new Product("cthulu", "img/chtulu.jpg");
+const dogduck = new Product("dog duck mouth", "img/dog-duck.jpg");
+const dragon = new Product("dragon meat", "img/dragon.jpg");
+const pen = new Product("pen lid utensils", "img/pen.jpg");
+const petsweep = new Product("pet sweep", "img/pet-sweep.jpg");
+const scissors = new Product("pizza scissors", "img/scissors.jpg");
+const shark = new Product("shark sleeping bag", "img/shark.jpg");
+const sweep = new Product("baby sweep", "img/sweep.jpg");
+const tauntaun = new Product("tauntaun sleeping bag", "img/tauntaun.jpg");
+const unicorn = new Product("unicorn meat", "img/unicorn.jpg");
+const watercan = new Product("self watering can", "img/water-can.jpg");
+const wineglass = new Product("wineglass", "img/wine-glass.jpg");
+
+renderProducts();
+
+productContainer.addEventListener("click", handleProductClick); 
+
 }
-
-
